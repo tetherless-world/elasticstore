3f0412b6df768c36bfb918ed84f76ea6ae425c76
Ethan Rambacher <rambae@dsa01.dynamic.rpi.edu>
2019-10-16 09:30:09 -0400

    rambae: Un-skipped GraphTestCases and ContextTestCases

--- test/test_elasticsearch_sparql.py
+++ test/test_elasticsearch_sparql.py
@@ -2,52 +2,45 @@ import logging
 import os
 import unittest
 
-from nose import SkipTest
 from rdflib import Literal
 
 from . import context_case
 from . import graph_case
 
-
-if os.environ.get("DB") != "sqlite":
-    raise SkipTest("SQLite not under test")
+from nose import SkipTest
 
 _logger = logging.getLogger(__name__)
 
-sqlalchemy_url = Literal(os.environ.get("DBURI", "sqlite://"))
+elasticsearch_url = Literal(os.environ.get("DBURI", "http://localhost:9200/collection"))
 
 
-class SQLASQLiteGraphTestCase(graph_case.GraphTestCase):
+class ElasticstoreGraphTestCase(graph_case.GraphTestCase):
     storetest = True
-    storename = "SQLAlchemy"
-    uri = sqlalchemy_url
+    storename = "Elasticsearch"
+    uri = elasticsearch_url
 
     def setUp(self):
-        super(SQLASQLiteGraphTestCase, self).setUp(
+        super(ElasticstoreGraphTestCase, self).setUp(
             uri=self.uri, storename=self.storename)
 
     def tearDown(self):
-        super(SQLASQLiteGraphTestCase, self).tearDown(uri=self.uri)
+        super(ElasticstoreGraphTestCase, self).tearDown(uri=self.uri)
 
 
-class SQLASQLiteContextTestCase(context_case.ContextTestCase):
+class ElasticstoreContextTestCase(context_case.ContextTestCase):
     storetest = True
-    storename = "SQLAlchemy"
-    uri = sqlalchemy_url
+    storename = "Elasticsearch"
+    uri = elasticsearch_url
 
     def setUp(self):
-        super(SQLASQLiteContextTestCase, self).setUp(
+        super(ElasticstoreContextTestCase, self).setUp(
             uri=self.uri, storename=self.storename)
 
     def tearDown(self):
-        super(SQLASQLiteContextTestCase, self).tearDown(uri=self.uri)
+        super(ElasticstoreContextTestCase, self).tearDown(uri=self.uri)
 
     def testLenInMultipleContexts(self):
         raise SkipTest("Known issue.")
-
-
-SQLASQLiteGraphTestCase.storetest = True
-SQLASQLiteContextTestCase.storetest = True
 
 
 if __name__ == "__main__":
