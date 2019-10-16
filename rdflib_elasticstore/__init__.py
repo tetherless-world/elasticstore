64c252daaeed3dbb0d30068307a5a0549b2a7b34
Ethan Rambacher <rambae@dsa01.dynamic.rpi.edu>
2019-10-16 09:10:30 -0400

    test_elasticsearch tests are valid (failing but correct)

--- rdflib_elasticstore/__init__.py
+++ rdflib_elasticstore/__init__.py
@@ -47,3 +47,31 @@ except ImportError:
 else:
     for ep in iter_entry_points(PLUGIN_ENTRY_POINT):
         CUSTOM_EVALS[ep.name] = ep.load()
+
+
+def registerplugins():
+    """
+    Register plugins.
+    If setuptools is used to install rdflib-elasticstore, all the provided
+    plugins are registered through entry_points. This is strongly recommended.
+    However, if only distutils is available, then the plugins must be
+    registed manually.
+    This method will register the rdflib-elasticstore Store plugin.
+    Adapted from rdflib-sqlalchemy on github:
+    https://github.com/RDFLib/rdflib-sqlalchemy/blob/develop/rdflib_sqlalchemy/__init__.py
+    """
+    from rdflib.store import Store
+    from rdflib import plugin
+    
+    try:
+        plugin.get("Elasticsearch", Store)
+    except plugin.PluginException:
+        pass
+    
+    # Register the plugins ...
+    plugin.register(
+        "Elasticsearch",
+        Store,
+        "rdflib_elasticstore.elasticstore",
+        "ElasticSearchStore",
+    )
